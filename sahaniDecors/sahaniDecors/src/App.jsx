5e46b9c932bcb125272a3d98f0c74b11e081c6a0
iori <92986739+dlnicoz@users.noreply.github.com>
2022-04-06 13:12:31 +0530

    test 1 for decors

---
+++ sahaniDecors/sahaniDecors/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+//import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="imagediv">
+          <div id="img1"></div>
+          <div id="img2"></div>
+          <div id="img3"></div>
+          <div id="img4"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
